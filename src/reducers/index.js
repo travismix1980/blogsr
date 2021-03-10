1fd6b80ce31d0a78931616c3109f6c1a8565e16b
Travis Mix <travismix1980@gmail.com>
2021-03-10 00:24:26 -0800

    Displaying Users now fetching them one at a time

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import postReducer from './postReducer';
+import usersReducer from './usersReducer';
 
 
 
 export default combineReducers({
 	posts: postReducer,
+	users: usersReducer,
 });
